5788189f7fcd4e96eb5fd6061be8ea7f5fab5c76
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-12 06:57:45 +0000

    Placeholder text replacement

--- components/simple_footer_component_1723445846.js
+++ components/simple_footer_component_1723445846.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1723445846", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            WantMaker - Manifest Your Desires
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        WantMaker helps you clarify and actualize your wants and goals. Join our community of dreamers and achievers today!
                     </div>
                 </div>
             </div>
